2189bd4956fe179a5a726623bac01105f34d8854
JVini0166 <63665043+JVini0166@users.noreply.github.com>
2021-09-26 20:52:32 -0300

    release v1.0
    Does:
    
    - Register Last Seen by name ("visto por último").
    - Fix bug to not catch ("online" messages, only "online" by tag).
    - Close driver, web browser and application in "Sair" button.
    
    Needs to improve -->
    
    - How to differ contact tab name from contact list name to know if it is on contact tab typed.

---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WhatsAppOnlineChecker
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver browser = new ChromeDriver();
+
+        public Form1()
+        {
+            InitializeComponent();
+            statusLabel.Text = "Coloque o nome do contato e prossiga.";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            whatsApp();
+            aindaEstaRodando();
+        }
+
+        public async void whatsApp()
+        {
+            browser.Url = "https://web.whatsapp.com/";
+            validaAbaDoContato();
+            registrarContatoEstavaOnline();
+        }
+
+        public string getCurrentTime()
+        {
+            String timeString;
+            return timeString = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        public string getNomeContato()
+        {
+            String nomeContato;
+            return nomeContato = textBox1.Text;
+        }
+
+        public async void aindaEstaRodando()
+        {
+            registroOnlineBox.Items.Add(getCurrentTime() + " relaxe, está rodando, +5min.");
+            await Task.Delay(300000); //Verificar a cada 5 minutos se está rodando
+            aindaEstaRodando();
+        }
+
+        public Boolean contemTagOnline()
+        {
+            String tagOnline = "title=\"online\"";
+            return browser.PageSource.Contains(tagOnline);
+        }
+
+        public async void validaAbaDoContato()
+        {
+            while (!(browser.PageSource.Contains(getNomeContato()) && (contemTagOnline() || browser.PageSource.Contains("visto por") || browser.PageSource.Contains("digitando"))))
+            {
+                statusLabel.Text = "Não está na aba algum contato.";
+                await Task.Delay(1000);
+            }
+            await Task.Delay(1000);
+            statusLabel.Text = "Está na aba de algum contato.";
+            validaAbaDoContato();
+        }
+
+        public async void registrarContatoEstavaOnline()
+        {
+            while (!(contemTagOnline()))
+            {
+                await Task.Delay(2000);
+            }
+
+            registroOnlineBox.Items.Add(getCurrentTime() + " " + getNomeContato() + " esteve online.");
+            await Task.Delay(2000);
+            registrarContatoEstavaOnline();
+        }
+
+        private void registroOnlineBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            browser.Quit();
+        }
+    }
+}
